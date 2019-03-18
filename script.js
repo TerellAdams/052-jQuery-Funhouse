7608771808508416acd1d5c355478e42bd358962
TerellAdams <42752944+TerellAdams@users.noreply.github.com>
2019-03-18 13:58:48 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(".pokeball").click(function() {
+    $(".eevee").fadeToggle();
+});
+
+$(".light-switch").click(function() {
+     $(".light-bulb").toggle();
+});
